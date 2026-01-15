6f231d8a41e92991f5c2524193608013873f4ff6
duudafs <dudascomparin@gmail.com>
2026-01-15 15:28:50 -0300

    novo commit

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,27 +3,48 @@ import './Login.css'
 function Login() {
    return (
   <div id="carouselExampleRide" className="carousel slide"data-bs-ride="true">
+
+     <div className="carousel-indicators">
+    <button data-bs-target="#carouselExample" data-bs-slide-to="0" className="active" />
+    <button data-bs-target="#carouselExample" data-bs-slide-to="1" />
+    <button data-bs-target="#carouselExample" data-bs-slide-to="2" />
+  </div>
     <div className="carousel-inner">
 
       <div className="carousel-item active">
         <div className="InitialCard">
-          <h2>Seja Bem vindo!</h2>
-          <h4>Travel trip</h4>
+            <div className="text-welcome">
+                <h2>Seja Bem vindo!</h2>
+                 <h4>Travel text</h4>
+            </div>
         </div>
       </div>
 
       <div className="carousel-item">
         <div className="InitialCard">
-          <h5>Planeje. viaje melhor. relembre sempre. </h5>
-          <h3>Crie roteiros inteligentes com ia e transforme cada viagem em um álbum único, do seu jeito.</h3>
+            <div className="card-about">
+ <div className="text-about">
+<h4>Planeje. viaje melhor. relembre sempre. </h4>
+          <h6>Crie roteiros inteligentes com ia e transforme cada viagem em um álbum único, do seu jeito.</h6>
+            </div>
+            </div>
         </div>
       </div>
 
-      <div className="carousel-item">
-        <div className="InitialCard">
-          <h5>Initial Card 3</h5>
+      <div className="carousel-item" style={{ backgroundColor: "rgba(216, 228, 250, 1)" }}>
+        <div className="LoginCard">
+           <div className="card-login">
+            <h3>Crie uma conta</h3>
+             <input type="text" placeholder="Nome" className="name-field" />
+        <input type="text" placeholder="Email" className="email-field" />
+           
+           </div>
+               
+        
         </div>
       </div>
+
+      
 
     </div>
 
