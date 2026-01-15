fd8db5a91dff4a50c368a87450b5c814aeb7311f
duudafs <dudascomparin@gmail.com>
2026-01-15 18:09:45 -0300

    registro

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -64,8 +64,8 @@ function Login() {
 </div>
 </div>
             </div>
-       
           </div>
+
         </div>
 
       </div>
--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,16 +1,27 @@
 import './Registro.css'
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 function Registro() {
+    const [isregister, setIsregister] = useState(true)
   return (
-    <div>
+    <div className="card-registro">
       <h2>Registro</h2>
-      <form>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+     <div class="row">
+    <div class="col-lg-8"> <input className="nome-input" type="text" placeholder="Nome" /></div>
+   
+  </div>
+  <div class="row">
+    <div class="col-lg-8"> <input className="sobrenome-input" type="text" placeholder="Sobrenome" />
+    </div>
+  
+  </div>
+       
+       
+        <input className="email-input" type="email" placeholder="Email" />
+        <input className="senha-input" type="password" placeholder="Senha" />
         <button type="submit">Registrar</button>
-      </form>
+     
     </div>
   )
 }
