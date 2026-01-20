3bc52fb0e886c25da4a6a2e67280c32ab60710ae
duudafs <dudascomparin@gmail.com>
2026-01-20 15:30:34 -0300

    registro page

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,29 +1,99 @@
-import './Registro.css'
+import "./Registro.css"
 import { Link } from "react-router-dom"
 import { useState } from "react"
 
 function Registro() {
-    const [isregister, setIsregister] = useState(true)
-  return (
-    <div className="card-registro">
-      <h2>Registro</h2>
-     <div class="row">
-    <div class="col-lg-8"> <input className="nome-input" type="text" placeholder="Nome" /></div>
-   
-  </div>
-  <div class="row">
-    <div class="col-lg-8"> <input className="sobrenome-input" type="text" placeholder="Sobrenome" />
-    </div>
-  
-  </div>
-       
-       
-        <input className="email-input" type="email" placeholder="Email" />
-        <input className="senha-input" type="password" placeholder="Senha" />
-        <button type="submit">Registrar</button>
-     
-    </div>
-  )
+const [nome, setNome] = useState("")
+const [sobrenome, setSobrenome] = useState("")
+const [uf, setUF] = useState("UF")
+const [cidade, setCidade] = useState("")
+const [email, setEmail] = useState("")
+const [senha, setSenha] = useState("")
+
+const handleChange = (event) =>{
+    setUF(event.target.value)
+}
+
+    return (
+        <div className="card-registro">
+            <h2>sign up</h2>
+
+            <form className="container-registro">
+
+                <div className="row">
+                    <div className="col">
+                        <label>Nome</label>
+                        <input
+                            type="text"
+                            placeholder="Nome..."
+                            value={nome}
+                            onChange={(e) => setNome(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="col">
+                        <label>Sobrenome</label>
+                        <input
+                            type="text"
+                            placeholder="Sobrenome..."
+                            value={sobrenome}
+                            onChange={(e) => setSobrenome(e.target.value)}
+                        />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col uf">
+                        <label>UF</label>
+                        <select value={uf} onChange={handleChange}>
+                            <option>UF</option>
+                            <option>AM</option>
+                            <option>SP</option>
+                            <option>RJ</option>
+                            <option>MG</option>
+                            <option>DF</option>
+                        </select>
+                    </div>
+
+                    <div className="col">
+                        <label>Cidade</label>
+                        <input type="text" 
+                        placeholder="Cidade..."
+                        value={cidade} 
+                        onChangeCapture={(e) => setCidade(e.target.value)}/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col full">
+                        <label>Email</label>
+                        <input type="email" 
+                        placeholder="Email..."
+                        value={email}
+                        onChangeCapture={(e) => setEmail(e.target.value)} />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col full">
+                        <label>Senha</label>
+                        <input type="password" 
+                        placeholder="Digite sua senha..."
+                        value={senha}
+                        onChangeCapture={(e) => setSenha(e.target.value)} />
+                        <span className="hint">A senha deve conter pelo menos 8 caracteres.</span>
+                    </div>
+                </div>
+
+                <button type="submit">Cadastrar</button>
+
+                <p className="login-text">
+                    Já tem uma conta? <Link to="/"><strong style={{ color: "rgb(71, 160, 130)" }}>fazer login</strong></Link>
+                </p>
+
+            </form>
+        </div>
+    )
 }
 
 export default Registro
