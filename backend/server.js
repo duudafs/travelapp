4a0780f619ab1f936ce29f327197597834bf08a3
duudafs <dudascomparin@gmail.com>
2026-01-20 17:55:22 -0300

    postman

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,34 @@ app.get('/api/teste', (req, res) => {
   res.json({ msg: 'api funcionando' })
 })
 
+app.post('/api/cadastro', (req, res) => {
+  const { nome, sobrenome, email, senha, cidade, uf } = req.body
+
+  if (!nome || !sobrenome || !email || !senha) {
+    return res.status(400).json({ msg: 'campos obrigatórios faltando' })
+  }
+
+
+  const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
+
+  if (!senhaRegex.test(senha)) {
+    return res.status(400).json({
+      msg: 'senha fraca'
+    })
+  }
+
+  res.status(201).json({
+    msg: 'usuário cadastrado com sucesso',
+    usuario: {
+      nome,
+      sobrenome,
+      email,
+      cidade,
+      uf
+    }
+  })
+})
+
 app.listen(3000, () => {
   console.log('backend rodando na porta 3000')
 })
